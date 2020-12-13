1b6dbd616ef14135e7ab175e20adb6a6bbf18d2f
sportswizsrinjoy <72151338+sportswizsrinjoy@users.noreply.github.com>
2020-12-13 17:08:43 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,43 @@
+class Game{
+    constructor(){
+
+
+    }
+    getState(){
+        var gameStateref=database.ref("gameState");
+        gameStateref.on("value",function(data){gameState=data.val()})
+
+    }
+  update(state){
+      database.ref("/").update({gameState:state})
+  }
+  start(){
+      if(gameState==0){
+          player=new Player();
+          player.getCount();
+          form=new Form();
+          form.display();
+      }
+  }
+  play(){
+      form.hide();
+      textSize(30);
+      text("game start",120,100);
+      Player.getPlayerInfo();
+      if(allPlayers!==undefined){
+       var displayPosition=130;
+       for(var plr in allPlayers){
+
+       
+       displayPosition=displayPosition+20;
+       textSize(15) ;
+       text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition);
+       }
+      }
+      if(keyIsDown(UP_ARROW)&&player.index!==null){
+          player.distance=player.distance+50;
+          player.update();
+      }     
+  }
+        
+    }
